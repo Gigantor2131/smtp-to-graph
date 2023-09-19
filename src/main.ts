8d59855da74aa0d3a94ceeec91042cfb9694bfd0
Travis Hatch <travishatch@epcs.com>
2023-09-18 19:45:33 -0600

    add DEBUG env flag to log body (HTML & Text)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { SMTPServer } from 'smtp-server'
-import { Client } from "@microsoft/microsoft-graph-client"
+import { Client } from '@microsoft/microsoft-graph-client'
 import { simpleParser as parser } from 'mailparser'
 import type { Message } from '@microsoft/microsoft-graph-types'
 import { ClientSecretCredential } from '@azure/identity'
-import { TokenCredentialAuthenticationProvider } from "@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials";
+import { TokenCredentialAuthenticationProvider } from '@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials';
 
 import { toAddress } from './utils'
 
@@ -15,18 +15,19 @@ const MSAL_TENANT_ID = process.env.MSAL_TENANT_ID
 const MSAL_CLIENT_ID = process.env.MSAL_CLIENT_ID
 const MSAL_CLIENT_SECRET = process.env.MSAL_CLIENT_SECRET
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN
+const DEBUG = process.env.DEBUG?.toUpperCase() === 'TRUE'
 const PORT = Number(process.env.PORT ?? 25)
 
 
-if (OVERIDE_FROM_ADDRESS === undefined || OVERIDE_FROM_ADDRESS === null || OVERIDE_FROM_ADDRESS === "") { console.log('OVERIDE_FROM_ADDRESS is not defined') } else { console.log(`OVERIDE_FROM_ADDRESS: ${OVERIDE_FROM_ADDRESS}`) }
+if (OVERIDE_FROM_ADDRESS === undefined || OVERIDE_FROM_ADDRESS === null || OVERIDE_FROM_ADDRESS === '') { console.log('OVERIDE_FROM_ADDRESS is not defined') } else { console.log(`OVERIDE_FROM_ADDRESS: ${OVERIDE_FROM_ADDRESS}`) }
 
 const graphClient = (() => {
-	if (ACCESS_TOKEN === undefined || ACCESS_TOKEN === null || ACCESS_TOKEN === "") {
+	if (ACCESS_TOKEN === undefined || ACCESS_TOKEN === null || ACCESS_TOKEN === '') {
 		console.log('ACCESS_TOKEN is not defined; using App Registration')
 
-		if (MSAL_TENANT_ID === undefined || MSAL_TENANT_ID === null || MSAL_TENANT_ID === "") { throw new Error('MSAL_TENANT_ID is not defined') } else { console.log(`MSAL_TENANT_ID: $${MSAL_TENANT_ID}`) }
-		if (MSAL_CLIENT_ID === undefined || MSAL_CLIENT_ID === null || MSAL_CLIENT_ID === "") { throw new Error('MSAL_CLIENT_ID is not defined') } else { console.log(`MSAL_CLIENT_ID: ${MSAL_CLIENT_ID}`) }
-		if (MSAL_CLIENT_SECRET === undefined || MSAL_CLIENT_SECRET === null || MSAL_CLIENT_SECRET === "") { throw new Error('MSAL_CLIENT_SECRET is not defined') } else { console.log('MSAL_CLIENT_SECRET: was provided') }
+		if (MSAL_TENANT_ID === undefined || MSAL_TENANT_ID === null || MSAL_TENANT_ID === '') { throw new Error('MSAL_TENANT_ID is not defined') } else { console.log(`MSAL_TENANT_ID: $${MSAL_TENANT_ID}`) }
+		if (MSAL_CLIENT_ID === undefined || MSAL_CLIENT_ID === null || MSAL_CLIENT_ID === '') { throw new Error('MSAL_CLIENT_ID is not defined') } else { console.log(`MSAL_CLIENT_ID: ${MSAL_CLIENT_ID}`) }
+		if (MSAL_CLIENT_SECRET === undefined || MSAL_CLIENT_SECRET === null || MSAL_CLIENT_SECRET === '') { throw new Error('MSAL_CLIENT_SECRET is not defined') } else { console.log('MSAL_CLIENT_SECRET: was provided') }
 
 		const credential = new ClientSecretCredential(
 			MSAL_TENANT_ID,
@@ -48,8 +49,8 @@ const graphClient = (() => {
 
 const smtpServer = new SMTPServer({
 	// secure: true,
-	// key: readFileSync(join(__dirname, "../certs/smtp_key.pem")),
-	// cert: readFileSync(join(__dirname, "../certs/smtp.crt")),
+	// key: readFileSync(join(__dirname, '../certs/smtp_key.pem')),
+	// cert: readFileSync(join(__dirname, '../certs/smtp.crt')),
 	// ca: [readFileSync(join(__dirname, '../certs/ca.crt'))],
 	// authMethods: ['PLAIN', 'LOGIN'],
 
@@ -82,16 +83,27 @@ const smtpServer = new SMTPServer({
 	onData(stream, session, callback) {
 		parser(stream)
 			.then(msg => {
-				console.dir({
-					rawFrom: msg.from?.value[0].address,
-					from: OVERIDE_FROM_ADDRESS || msg.from?.value[0].address,
-					to: msg.to,
-					subject: msg.subject,
-					isHtml: msg.html === false,
-					html: String(msg.html),
-					text: msg.text,
-					attachments: msg.attachments.length,
-				}, { depth: 5 })
+				if (DEBUG) {
+					console.dir({
+						rawFrom: msg.from?.value[0].address,
+						from: OVERIDE_FROM_ADDRESS || msg.from?.value[0].address,
+						to: msg.to,
+						subject: msg.subject,
+						isHtml: msg.html === false,
+						html: String(msg.html),
+						text: msg.text,
+						attachments: msg.attachments.length,
+					}, { depth: 5 })
+				}
+				else {
+					console.dir({
+						rawFrom: msg.from?.value[0].address,
+						to: toAddress(msg.to),
+						subject: msg.subject,
+						isHtml: msg.html !== false,
+						attachments: msg.attachments.length,
+					}, { depth: 5 })
+				}
 
 				// map from smtp to graph
 				const sendMail: { message: Message, saveToSentItems?: boolean } = {
@@ -110,7 +122,7 @@ const smtpServer = new SMTPServer({
 						ccRecipients: toAddress(msg.cc),
 						bccRecipients: toAddress(msg.bcc),
 						attachments: msg.attachments.map(att => ({
-							"@odata.type": "#microsoft.graph.fileAttachment",
+							'@odata.type': '#microsoft.graph.fileAttachment',
 							name: att.filename,
 							contentType: att.contentType,
 							contentBytes: att.content.toString('base64'),
